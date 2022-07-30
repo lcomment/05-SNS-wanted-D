ea8e01fa777d6b7931d480d43377384ba2f768d1
lcomment <kohyunsuk98@gmail.com>
2022-07-30 17:43:22 +0900

    test: 간단한 테스트 코드 작성

---
+++ src/main/java/com/wanted/sns/domain/posts/dao/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.wanted.sns.domain.posts.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
