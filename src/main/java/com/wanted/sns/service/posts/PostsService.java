7ada2d59bfa2773d0f3cae591ad2eb6da5403f00
lcomment <kohyunsuk98@gmail.com>
2022-08-05 00:25:57 +0900

    posts 삭제 수정

--- src/main/java/com/wanted/sns/service/posts/PostsService.java
+++ src/main/java/com/wanted/sns/service/posts/PostsService.java
@@ -1,5 +1,6 @@
 package com.wanted.sns.service.posts;
 
+import com.wanted.sns.controller.dto.PostsListResponseDto;
 import com.wanted.sns.controller.dto.PostsResponseDto;
 import com.wanted.sns.controller.dto.PostsSaveRequestDto;
 import com.wanted.sns.controller.dto.PostsUpdateRequestDto;
@@ -8,6 +9,9 @@ import com.wanted.sns.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다. id=" + id));
 
         return new PostsResponseDto(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
     }
 }
